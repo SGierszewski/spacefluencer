29c1bb909787fe9d9dbfe484b0218216f0d20fbf
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-07 13:41:34 +0200

    ✨ Add page layout

--- main.js
+++ main.js
@@ -1,6 +1,67 @@
 import "./style.css";
+import { createElement } from "./utils/elements";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const headerElement = createElement("header", {
+  className: "header",
+  children: [
+    createElement("h1", {
+      className: "title",
+      innerText: "Spacefluencer",
+    }),
+  ],
+});
+
+const mainElement = createElement("main", {
+  className: "main",
+  children: [
+    createElement("h2", {
+      className: "main-title",
+      innerText: "Meet your favorite Spacefluencer",
+    }),
+    createElement("section", {
+      className: "characters",
+      children: [
+        createElement("div", {
+          className: "characters-intro",
+          innerText: "Display popular Spacefluencer here...",
+        }),
+      ],
+    }),
+  ],
+});
+
+const footerElement = createElement("footer", {
+  className: "footer",
+  children: [
+    createElement("div", {
+      className: "footer-info",
+      children: [
+        createElement("span", {
+          className: "footer-info__text",
+          innerText: "Made with ",
+        }),
+        createElement("span", {
+          className: "footer-info_logo",
+          innerText: "❤",
+        }),
+        createElement("span", {
+          className: "footer-info_text",
+          innerText: " and ",
+        }),
+        createElement("span", {
+          className: "footer-info_link",
+          children: [
+            createElement("a", {
+              href: "https://api.nasa.gov/",
+              innerText: "NASA APIs",
+            }),
+          ],
+        }),
+      ],
+    }),
+  ],
+});
+
+document
+  .querySelector("#app")
+  .append(headerElement, mainElement, footerElement);
