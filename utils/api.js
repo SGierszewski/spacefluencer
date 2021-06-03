7377f52db49b6fa14c82bd201952fb8a32b0004c
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-03 18:57:03 +0200

    🔒️  Import API Key from .env

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,9 @@
 // Get rover data from API
 export async function getPhotos(rover, date) {
   const promise = fetch(
-    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=WOyfELI8lB2BmbG3Y6doTsCTqYqLP1L0twnkeIIy&earth_date=${date}`
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=${
+      import.meta.env.VITE_API_KEY
+    }&earth_date=${date}`
   );
   const response = await promise;
   if (response.status === 404) {
@@ -13,7 +15,9 @@ export async function getPhotos(rover, date) {
 
 export async function getRoverInfo(name) {
   const promise = fetch(
-    `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}?api_key=WOyfELI8lB2BmbG3Y6doTsCTqYqLP1L0twnkeIIy`
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}?api_key=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
   const response = await promise;
   if (response.status === 404) {
